ad2bbefdb2dcce3d7e5a0183363c75429a018c8e
Tomoki Imai <tomo832@gmail.com>
2019-01-29 19:29:13 +0900

    Add color palette

--- src/bootpack.c
+++ src/bootpack.c
@@ -1,12 +1,58 @@
 void io_hlt(void);
-void write_mem8(int addr, int data);
+void io_cli(void);
+int io_load_eflags(void);
+void io_store_eflags(int eflags);
+void io_out8(int port, int data);
 
+void set_pallete(int start, int end, unsigned char* rgb)
+{
+    int backup_eflags = io_load_eflags();
+    io_cli(); // prevent interruption
+
+    int k = 0;
+    for (int i = start; i <= end; i++) {
+        // Note that each color should be 6 bit.
+        io_out8(0x03c8, i);
+        io_out8(0x03c9, rgb[3 * k + 0] >> 2);
+        io_out8(0x03c9, rgb[3 * k + 1] >> 2);
+        io_out8(0x03c9, rgb[3 * k + 2] >> 2);
+        k++;
+    }
+
+    io_store_eflags(backup_eflags);
+}
+
+void init_palette(void)
+{
+    static unsigned char table_rgb[16 * 3] = {
+        0x00, 0x00, 0x00, // 0: black
+        0xff, 0x00, 0x00, // 1: red
+        0x00, 0xff, 0x00, // 2: green
+        0x00, 0x00, 0xff, // 3: blue
+        0xff, 0xff, 0x00, // 4: yellow
+        0xff, 0x00, 0xff, // 5: Purple
+        0x00, 0xff, 0xff, // 6: light blur
+        0xff, 0xff, 0xff, // 7: white
+        0xc6, 0xc6, 0xc6, // 8: gray
+        0xc6, 0x00, 0x00, // 9: dark red
+        0x00, 0xc6, 0x00, // 10: dark green
+        0x00, 0x00, 0xc6, // 11: dark blue
+        0xc6, 0xc6, 0x00, // 12: dark yellow
+        0xc6, 0x00, 0xc6, // 13: dark Purple
+        0x00, 0xc6, 0xc6, // 14: dark light blur
+        0xc6, 0xc6, 0xc6, // 15: dark white
+    };
+
+    set_pallete(0, 15, table_rgb);
+}
 
 void MilfaMain(void)
 {
+    init_palette();
     // 0xa0000 ~ 0xaffff is framebuffer
-    for (int i = 0xa0000; i <= 0xaffff; i++)
-        write_mem8(i, 10);
+    for (int i = 0xa0000; i <= 0xaffff; i++) {
+        * ((char*)i) = i & 0x0f;
+    }
 
 fin:
     io_hlt();
--- src/nasmfunc.nasm
+++ src/nasmfunc.nasm
@@ -1,5 +1,17 @@
 [BITS 32]
 global io_hlt
+global io_cli
+global io_sti
+global io_stihlt
+global io_in8
+global io_in16
+global io_in32
+global io_out8
+global io_out6
+global io_out32
+
+global io_store_eflags
+global io_load_eflags
 global write_mem8
 
 [SECTION .text]
@@ -8,9 +20,68 @@ io_hlt:
     hlt
     ret
 
-; void write_mem8(int addr, int data)
-write_mem8:
-    mov ecx, [esp+4]
-    mov al, [esp+8]
-    mov [ecx], al
+; void io_cli(void)
+; disallow interrupt
+io_cli:
+    cli
+    ret
+
+; void io_sti(void)
+; allow interrupt
+io_sti:
+    sti
+    ret
+
+; int io_in8(int port)
+io_in8:
+    mov edx, [esp+4]
+    mov eax, 0
+    in al, dx
+    ret
+
+; int in_in16(int port)
+io_in16:
+    mov edx, [esp+4]
+    mov eax, 0
+    in ax, dx
+    ret
+
+; int in_in16(int port)
+io_in32:
+    mov edx, [esp+4]
+    in eax, dx
+    ret
+
+; void io_out8(int port, int data)
+io_out8:
+    mov edx, [esp+4]
+    mov eax, [esp+8]
+    out dx, al
+    ret
+
+; void io_out16(int port, int data)
+io_out16:
+    mov edx, [esp+4]
+    mov eax, [esp+8]
+    out dx, ax
+    ret
+
+; void io_out32(int port, int data)
+io_out32:
+    mov edx, [esp+4]
+    mov eax, [esp+8]
+    out dx, eax
+    ret
+
+; int io_load_eflags(void)
+io_load_eflags:
+    pushfd ; push eflags
+    pop eax
+    ret
+
+; void io_store_eflags(int)
+io_store_eflags:
+    mov eax, [esp+4]
+    push eax
+    popfd ; pop eflags
     ret
